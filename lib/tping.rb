a9db2d92416e96337578281f9f376db2a04aa10f
Ryan Duryea <aguynamedryan@gmail.com>
2017-09-08 17:48:53 -0700

    Initial commit

---
+++ lib/tping.rb
@@ -0,0 +1,19 @@
+require_relative "tping/version"
+
+module Tping
+  def self.request_build(token, user, repo, pro = false)
+    headers = ["Content-Type: application/json",
+               "Accept: application/json",
+               "Travis-API-Version: 3",
+               "Authorization: token #{token}"].flat_map do |header|
+      ["-H", header]
+    end
+
+    command = %w(curl -s -X POST)
+    command += headers
+    command += ["-d", '{ "request": { "branch": "master" }}']
+    command <<  "https://api.travis-ci.#{pro ? "com" : "org"}/repo/#{user}%2F#{repo}/requests"
+    p command
+    system(*command)
+  end
+end
---
+++ lib/tping/version.rb
@@ -0,0 +1,3 @@
+module Tping
+  VERSION = "0.0.1"
+end
